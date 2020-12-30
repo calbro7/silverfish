9b458ae6c15686737deec77ad4a515988c247bec
Callum Browne <callum@callumbrowne.me>
2020-12-30 16:30:18 +0000

    Refactor UCI into proper struct with injected output stream

--- src/main.rs
+++ src/main.rs
@@ -14,51 +14,10 @@ mod tests;
 use text_io::read;
 
 fn main() {
-    let mut state = state::State::start_pos();
+    let mut uci = uci::UciHandler::new(std::io::stdout());
 
     loop {
-        let command: String = read!("{}\n");
-        
-        if command.starts_with("isready") {
-            println!("readyok");
-        }
-        else if command.starts_with("ucinewgame") {
-            state = state::State::start_pos();
-        }
-        else if command.starts_with("position") {
-            match uci::parse_position_command(&command) {
-                Ok(s) => {
-                    state = s;
-                },
-                _ => {}
-            };
-        }
-        else if command.starts_with("go") {
-            let mut moves = moves::generate_moves(&state);
-            while !moves.is_empty() {
-                let r#move = moves.pop();
-                match state.make_move(r#move) {
-                    Ok(_) => {
-                        println!("bestmove {}{}", helpers::sq_to_algebraic(moves::move_from(r#move)), helpers::sq_to_algebraic(moves::move_to(r#move)));
-                        break
-                    },
-                    Err(_) => {}
-                };
-            }
-        }
-        else if command.starts_with("uci") {
-            println!("id silverfish");
-            println!("uciok");
-        }
-        else if command.starts_with("quit") {
-            break;
-        }
-        else if command.starts_with("perft") {
-            let depth: u8 = command.split_whitespace().skip(1).next().unwrap().parse().unwrap();
-            println!("{}", perft::perft(&mut state, depth, true));
-        }
-        else if command.starts_with("eval") {
-            println!("{}", eval::eval(&state))
-        }
+        let input: String = read!("{}\n");
+        uci.command(&input);
     }
 }
--- src/uci.rs
+++ src/uci.rs
@@ -1,75 +1,161 @@
 use crate::state::State;
-use crate::helpers::algebraic_to_sq;
+use crate::helpers::{algebraic_to_sq, sq_to_algebraic};
 use crate::moves::{BitMove, encode_move, move_from, move_to, move_promotion_piece, generate_moves};
 use crate::pieces::Piece;
+use crate::colours::Colour;
+use crate::perft::perft;
+use crate::eval::eval;
 use std::str::SplitWhitespace;
+use std::process::exit;
+use std::io::{Write};
 
-pub struct InvalidUciCommandError;
+pub struct UciHandler<W: Write> {
+    state: State,
+    out: W
+}
 
-pub fn parse_position_command(command: &str) -> Result<State, InvalidUciCommandError> {
-    let mut segments = command.split_whitespace().skip(1);
-    let mut state = match segments.next() {
-        Some("startpos") => State::start_pos(),
-        Some("fen") => {
-            let mut fen = String::new();
-            for i in 0..6 {
-                match segments.next() {
-                    Some(s) => fen.push_str(&(" ".to_owned() + s)),
-                    None => {
-                        return Err(InvalidUciCommandError);
-                    }
-                }
-            }
-            fen = fen.trim().to_string();
-            match State::from_fen(&fen) {
-                Ok(s) => s,
-                Err(_) => {
-                    return Err(InvalidUciCommandError);
-                }
-            }
-        },
-        _ => {
-            return Err(InvalidUciCommandError);
+impl<W> UciHandler<W> where W: Write {
+    pub fn new(out: W) -> Self {
+        Self {
+            state: State::start_pos(),
+            out: out
         }
-    };
+    }
 
-    if let Some("moves") = segments.next() {
-        loop {
-            match segments.next() {
-                Some(move_string) => {
-                    if move_string.len() < 4 {
-                        return Err(InvalidUciCommandError);
+    pub fn command(&mut self, command: &str) {
+        if command.starts_with("isready") {
+            self.isready();
+        }
+        else if command.starts_with("ucinewgame") {
+            self.ucinewgame();
+        }
+        else if command.starts_with("position") {
+            self.position(command);
+        }
+        else if command.starts_with("go") {
+            self.go(command);
+        }
+        else if command.starts_with("uci") {
+            self.uci();
+        }
+        else if command.starts_with("quit") {
+            self.quit();
+        }
+        else if command.starts_with("perft") {
+            self.perft(command);
+        }
+        else if command.starts_with("eval") {
+            self.eval();
+        }
+    }
+
+    fn isready(&mut self) {
+        writeln!(&mut self.out, "readyok");
+    }
+
+    fn ucinewgame(&mut self,) {
+        self.state = State::start_pos();
+    }
+
+    fn position(&mut self, command: &str) {
+        let mut segments = command.split_whitespace().skip(1);
+        let mut state = match segments.next() {
+            Some("startpos") => State::start_pos(),
+            Some("fen") => {
+                let mut fen = String::new();
+                for i in 0..6 {
+                    match segments.next() {
+                        Some(s) => fen.push_str(&(" ".to_owned() + s)),
+                        None => {
+                            return;
+                        }
                     }
-
-                    let mut move_list = generate_moves(&state);
-
-                    let from = algebraic_to_sq(&move_string[0..2]);
-                    let to = algebraic_to_sq(&move_string[2..4]);
-                    let promotion_piece = if move_string.len() == 5 {
-                        match &move_string[4..5] {
-                            "n" => Some(Piece::Knight),
-                            "b" => Some(Piece::Bishop),
-                            "r" => Some(Piece::Rook),
-                            "q" => Some(Piece::Queen),
-                            _ => {
-                                return Err(InvalidUciCommandError);
+                }
+                fen = fen.trim().to_string();
+                match State::from_fen(&fen) {
+                    Ok(s) => s,
+                    Err(_) => {
+                        return;
+                    }
+                }
+            },
+            _ => {
+                return;
+            }
+        };
+    
+        if let Some("moves") = segments.next() {
+            loop {
+                match segments.next() {
+                    Some(move_string) => {
+                        if move_string.len() < 4 {
+                            return;
+                        }
+    
+                        let mut move_list = generate_moves(&state);
+    
+                        let from = algebraic_to_sq(&move_string[0..2]);
+                        let to = algebraic_to_sq(&move_string[2..4]);
+                        let promotion_piece = if move_string.len() == 5 {
+                            match &move_string[4..5] {
+                                "n" => Some(Piece::Knight),
+                                "b" => Some(Piece::Bishop),
+                                "r" => Some(Piece::Rook),
+                                "q" => Some(Piece::Queen),
+                                _ => {
+                                    return;
+                                }
+                            }
+                        } else { None };
+    
+                        while !move_list.is_empty() {
+                            let r#move = move_list.pop();
+                            if from == move_from(r#move) && to == move_to(r#move) && promotion_piece == move_promotion_piece(r#move) {
+                                state.make_move(r#move);
+                                break;
                             }
                         }
-                    } else { None };
+                    },
+                    None => break
+                }
+            }
+        }
+        
+        self.state = state;
+    }
 
-                    while !move_list.is_empty() {
-                        let r#move = move_list.pop();
-                        if from == move_from(r#move) && to == move_to(r#move) && promotion_piece == move_promotion_piece(r#move) {
-                            state.make_move(r#move);
-                            break;
-                        }
-                    }
+    fn go(&mut self, command: &str) {
+        let mut moves = generate_moves(&self.state);
+        while !moves.is_empty() {
+            let r#move = moves.pop();
+            match self.state.make_move(r#move) {
+                Ok(_) => {
+                    writeln!(&mut self.out, "bestmove {}{}", sq_to_algebraic(move_from(r#move)), sq_to_algebraic(move_to(r#move)));
+                    break;
                 },
-                None => break
+                Err(_) => {}
             }
         }
     }
 
-    Ok(state)
-}
+    fn uci(&mut self) {
+        writeln!(&mut self.out, "id name silverfish");
+        writeln!(&mut self.out, "uciok");
+    }
 
+    fn quit(&self) {
+        exit(0);
+    }
+
+    fn perft(&mut self, command: &str) {
+        let depth: u8 = command.split_whitespace().skip(1).next().unwrap().parse().unwrap();
+        writeln!(&mut self.out, "{}", perft(&mut self.state, depth, true));
+    }
+
+    fn eval(&mut self) {
+        writeln!(&mut self.out, "{}", eval(&self.state) * match self.state.to_move {
+            Colour::White => 1,
+            Colour::Black => -1
+        });
+    }
+}
