fe715c7c3480458d89b0f6088d3358574b3d8cd4
Callum Browne <callum@callumbrowne.me>
2021-01-02 00:34:00 +0000

    Slight tidy up

--- src/search.rs
+++ src/search.rs
@@ -9,7 +9,7 @@ const MATE_VALUE: isize = 10000;
 
 pub fn search<W: std::io::Write>(mut state: &mut State, depth: Option<u8>, out: Option<&mut W>) -> (BitMove, isize) {
     let default_depth = 5u8;
-    let mut best: (BitMove, isize) = (0, -1 * MATE_VALUE);
+    let mut best: (BitMove, isize) = (0, -MATE_VALUE);
     let mut moves = generate_moves(&state);
     let mut node_counter = 0;
     while !moves.is_empty() {
@@ -18,7 +18,7 @@ pub fn search<W: std::io::Write>(mut state: &mut State, depth: Option<u8>, out: 
         if state.make_move(r#move).is_err() {
             continue;
         }
-        let score = -1 * negamax(&mut state, depth.unwrap_or(default_depth), -1 * MATE_VALUE, MATE_VALUE, 0, &mut node_counter);
+        let score = -negamax(&mut state, depth.unwrap_or(default_depth), -MATE_VALUE, MATE_VALUE, 0, &mut node_counter);
         if score >= best.1 {
             best.0 = r#move;
             best.1 = score;
@@ -28,7 +28,7 @@ pub fn search<W: std::io::Write>(mut state: &mut State, depth: Option<u8>, out: 
 
     let absolute_score = match state.to_move {
         Colour::White => best.1,
-        Colour::Black => -1 * best.1
+        Colour::Black => -best.1
     };
 
     if out.is_some() {
@@ -54,7 +54,7 @@ fn negamax(mut state: &mut State, depth: u8, mut alpha: isize, beta: isize, ply:
             continue;
         }
         num_legal_moves += 1;
-        let score = -1*negamax(&mut state, depth-1, -1*beta, -1*alpha, ply+1, &mut node_counter);
+        let score = -negamax(&mut state, depth-1, -beta, -alpha, ply+1, &mut node_counter);
         *state = copy;
         if score >= beta {
             return beta;
@@ -68,7 +68,7 @@ fn negamax(mut state: &mut State, depth: u8, mut alpha: isize, beta: isize, ply:
         let king_sq = get_ls1b(state.pieces[Piece::King as usize] & state.colours[state.to_move as usize]);
 
         if state.square_attacked(king_sq, !state.to_move) {
-            (-1 * MATE_VALUE) + (ply as isize)
+            (-MATE_VALUE) + (ply as isize)
         }
         else {
             0
